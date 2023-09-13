33e150769efef2a99861d7cefe02b6fafb85c4c2
Andy Tran <AyyyTran@gmail.com>
2023-09-13 16:49:28 -0700

    learning server and ports

--- modules.js
+++
@@ -1,4 +0,0 @@
-// require finds the file and runs it
-const {people, ages} = require('./people');
-//empty object unless module exports
-console.log(people, ages);
--- people.js
+++
@@ -1,7 +0,0 @@
-const people = ['yuoshi', 'ryu', 'chun-li', 'mario'];
-const ages = [20, 13, 12, 21];
-
-module.exports = {
-  people,
-  ages,
-};

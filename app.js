1dd0ced57c5c14847e459a81e7bb6c658ef08854
Andy Tran <AyyyTran@gmail.com>
2023-09-16 11:52:38 -0700

    get, post, delete of blogs. Details of blogs

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ mongoose
 app.set('view engine', 'ejs');
 
 //middleware and static files
-
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
@@ -37,8 +37,44 @@ app.get('/blogs', (req, res) => {
       console.log(err);
     });
 });
+
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', {title: 'Create a new Blog'});
+});
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({redirect: '/blogs'});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render('details', {blog: result, title: 'Blog Details'});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.use((req, res) => {
